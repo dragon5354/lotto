f4e0eeab49e48380f98dde14096374ea9088bdab
dragon5354 <dragon5354@naver.com>
2023-09-28 20:58:15 +0900

    랜덤 번호생성

--- app/src/main/java/com/example/lotto/MainActivity.kt
+++ app/src/main/java/com/example/lotto/MainActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.cardview.widget.CardView
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,20 +17,73 @@ class MainActivity : AppCompatActivity() {
         var constellationCard : CardView = findViewById<CardView>(R.id.constellationCard)
         var nameCard : CardView = findViewById<CardView>(R.id.nameCard)
 
-
-        // 랜덤 번호 생성. 바로 결과창으로
+        // 1. 랜덤 번호 생성. 바로 결과창으로
         randomCard.setOnClickListener {
-            startActivity(Intent(this,ResultActivity::class.java))
+
+            // ResultActivity를 시작하는 Intent 생성
+            val intent = Intent(this,ResultActivity::class.java)
+            // intent의 결과 데이터를 전달. 리스트를 전달하기 때문에 저런 형식으로 쓴다.
+            intent.putIntegerArrayListExtra("result", ArrayList(getRandomLottoNumbers()))
+
+            //intent를 만들고 startActivity로 실행
+            startActivity(intent)
         }
 
-        // 별자리 번호 생성. 해당 페이지로
+        // 2. 별자리 번호 생성. 해당 페이지로
         constellationCard.setOnClickListener {
             startActivity(Intent(this,ConstellationActivity::class.java))
         }
 
-        //이름 번호 생성. 해당 페이지로2
+        // 3. 이름 번호 생성. 해당 페이지로2
         nameCard.setOnClickListener {
             startActivity(Intent(this,NameActivity::class.java))
         }
+    }
+
+    /**
+     * 랜덤함수에 사용되는 부분
+     */
+    // 랜덤으로 1~45 번호중 하나의 번호를 생성하는 함수
+    fun getRandomLottoNumber() : Int {
+        // Random.nextInt는 0~전달받은 값 미만의 번호를 생성하므로, 1을 더해준다.
+        return Random().nextInt(45) + 1
+    }
+
+//    // 랜덤으로 6개의 로또 번호를 만드는 함수(랜덤함수 6번실행)
+//    fun getRandomLottoNumbers() : MutableList<Int> {
+//        // 리스트 생성
+//        val lottoNumbers = mutableListOf<Int>()
+//
+//        // 6번 반복
+//        for (i in 1..6) {
+//            // 임시로 사용할 변수. 중복 방지용
+//            var number = 0
+//            do {
+//                // 랜덤한 번호를 추출해 number에 저장. 중복 방지용
+//                number = getRandomLottoNumber()
+//
+//            } // lottoNumbers에 number 변수값이 없을때까지 반복
+//            while (lottoNumbers.contains(number))
+//            // 리스트에 생성한 번로를 추가
+//            lottoNumbers.add(getRandomLottoNumber())
+//        }
+//        return lottoNumbers
+//    }
+
+    // 셔플 (위쪽 주석 대신 사용가능)
+    fun getRandomLottoNumbers() : MutableList<Int> {
+        // 리스트 생성
+        val list = mutableListOf<Int>()
+
+        // 6번 반복
+        for (number in 1..45) {
+            list.add(number)
+        }
+
+        // 리스트 무작위로 섞기
+        list.shuffle()
+
+        // 리스트를 앞에서부터 순서대로 6개 잘라서 결과 반환
+        return list.subList(0,6)
     }
 }
--- app/src/main/java/com/example/lotto/ResultActivity.kt
+++ app/src/main/java/com/example/lotto/ResultActivity.kt
@@ -2,10 +2,52 @@ package com.example.lotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.cardview.widget.CardView
 
 class ResultActivity : AppCompatActivity() {
+
+    // 로또 1번 공 이미지의 id를 사용
+    // onCreate 밖에서 findViewById 같은 걸 놓게 되면 에러뜸
+    val lottoImageStartId = R.drawable.ball_01
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        // MainActivity에서 전달받은 결과 배열을 가져옴
+        val result = intent.getIntegerArrayListExtra("result")
+
+        // 전달받은 결과가 있는 경우에만 실행
+        result?.let {
+            // 결과에 맞게 공 이미지 업데이트. 전달을 위해 정렬도 넣는다.
+            updateLottoBallImage(result.sortedBy { it })
+        }
+    }
+
+    /**
+     * 결과에 따라 로또 공 이미지를 업데이트
+     */
+    fun updateLottoBallImage(result: List<Int>) {
+
+        val lottoImageStartId = R.drawable.ball_01
+        // '코틀린 안드로이드 익스텐션' 이 2021년부터 지원되지 않기 때문에, 일단 JAVA처럼 임시로 findViewById를 썼음
+        var imageView01 : ImageView = findViewById<ImageView>(R.id.imageView01)
+        var imageView02 : ImageView = findViewById<ImageView>(R.id.imageView02)
+        var imageView03 : ImageView = findViewById<ImageView>(R.id.imageView03)
+        var imageView04 : ImageView = findViewById<ImageView>(R.id.imageView04)
+        var imageView05 : ImageView = findViewById<ImageView>(R.id.imageView05)
+        var imageView06 : ImageView = findViewById<ImageView>(R.id.imageView06)
+
+        // 결과가 6개 미만일 경우 에러가 발생할 수 있어서 바로 리턴해버림
+        if(result.size < 6) return
+
+        // ball_01부터 이미지 id가 있으므로, 결과값-1이 목표 이미지가 됨
+        imageView01.setImageResource(lottoImageStartId + (result[0] - 1))
+        imageView02.setImageResource(lottoImageStartId + (result[1] - 1))
+        imageView03.setImageResource(lottoImageStartId + (result[2] - 1))
+        imageView04.setImageResource(lottoImageStartId + (result[3] - 1))
+        imageView05.setImageResource(lottoImageStartId + (result[4] - 1))
+        imageView06.setImageResource(lottoImageStartId + (result[5] - 1))
     }
 }
