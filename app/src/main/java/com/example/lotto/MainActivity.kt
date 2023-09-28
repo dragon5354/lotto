50d78e1c5d44e47647eaef2f59955d28c2b11aba
dragon5354 <dragon5354@naver.com>
2023-09-28 18:20:47 +0900

    결과화면, main 화면전환기능

--- app/src/main/java/com/example/lotto/MainActivity.kt
+++ app/src/main/java/com/example/lotto/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.lotto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // '코틀린 안드로이드 익스텐션' 이 2021년부터 지원하지 않기 때문에, 일단 JAVA처럼 임시로 findViewById를 썼음
+        var randomCard : CardView = findViewById<CardView>(R.id.randomCard)
+        var constellationCard : CardView = findViewById<CardView>(R.id.constellationCard)
+        var nameCard : CardView = findViewById<CardView>(R.id.nameCard)
+
+
+        // 랜덤 번호 생성
+        randomCard.setOnClickListener {
+            startActivity(Intent(this,ResultActivity::class.java))
+        }
+
+        // 별자리 번호 생성
+        constellationCard.setOnClickListener {
+            startActivity(Intent(this,ConstellationActivity::class.java))
+        }
+
+        //이름 번호 생성
+        nameCard.setOnClickListener {
+            startActivity(Intent(this,NameActivity::class.java))
+        }
     }
 }
